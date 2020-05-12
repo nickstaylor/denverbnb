40a0f47c857a6b7742f16e2c88c9f7d3d714f194
Quinn Elder <qre.turing@gmail.com>
2020-05-12 17:30:16 -0600

    Add refactoring - App Function component to be a Class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './App.css';
-import { NavLink, Redirect } from 'react-router-dom';
 
-function App() {
+import Login from '../Login/Login'
+
+import { NavLink, Switch, Route } from 'react-router-dom';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
   return (
     <section className="App">
-    <header>
-    <h1>denverbnb</h1>
-    <nav className="nav-container">
-       <NavLink to='/areas' className='nav'> All Neighborhoods </NavLink>
-       <NavLink to='/favorites' className='nav'> Favorites </NavLink>
-       <NavLink to='/' exact className='nav'> Sign Out </NavLink>
-     </nav>
-     </header>
+      <header>
+        <h1>denverbnb</h1>
+        <nav className="nav-container">
+          <NavLink to='/areas' className='nav'> All Neighborhoods </NavLink>
+          <NavLink to='/favorites' className='nav'> Favorites </NavLink>
+          <NavLink to='/' exact className='nav'> Sign Out </NavLink>
+        </nav>
+      </header>
+      <Switch>
+        <Route path="/" exact component={ Login }/>
+      </Switch>
     </section>
-  );
+  )
+ }
 }
 
 export default App;
