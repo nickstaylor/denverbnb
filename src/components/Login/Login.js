2293a4c122827866f7b15102418787a5be19f9ba
Quinn Elder <qre.turing@gmail.com>
2020-05-12 17:31:33 -0600

    Structure Login directory with all Login-files

---
+++ src/components/Login/Login.js
@@ -0,0 +1,17 @@
+import React, {
+  Component
+} from 'react'
+
+class Login extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return ( 
+     <h2> Hello world </h2>
+    )
+  }
+}
+export default Login
