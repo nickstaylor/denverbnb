db79612abba826956aac8aa3232fd7fd92d80caf
Nick Taylor <nickstaylor@gmail.com>
2020-05-12 16:28:32 -0600

    adding nav bar styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <section className="App">
-    <nav>
+    <header>
     <h1>denverbnb</h1>
+    <nav className="nav-container">
        <NavLink to='/areas' className='nav'> All Neighborhoods </NavLink>
        <NavLink to='/favorites' className='nav'> Favorites </NavLink>
-       <NavLink to='/' className='nav'> Sign Out </NavLink>
+       <NavLink to='/' exact className='nav'> Sign Out </NavLink>
      </nav>
+     </header>
     </section>
   );
 }
