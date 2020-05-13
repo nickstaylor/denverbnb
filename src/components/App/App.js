c59da6039c51965bf93feed810aca87c8a730733
December G <dgarnetsmith@gmail.com>
2020-05-13 17:06:08 -0600

    Add user to app state if conditions are met and redirect to home page if sign out button is clicked

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import Login from '../Login/Login'
 import Header from '../Header/Header'
-import { NavLink, Switch, Route } from 'react-router-dom';
+import AreaContainer from "../AreaContainer/AreaContainer"
+
+import { NavLink, Switch, Route, Redirect } from 'react-router-dom';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      userName: null,
+      user: '',
     }
+  }
+
+  addUser = (person) => {
+    this.setState({user: person})
+  }
+
+  removeUser = () => {
+    this.setState({user: ''})
   }
 
   //updates state to include current user's name and purpose
 
   render() {
+ 
   return (
     <section className="App">
       
       <Switch>
-        <Route path="/" exact render={ () => <Login />}/>
+        <Route path="/" exact render={ () => <Login addUser={this.addUser}/>}/>
+        {/* {this.state.user !== '' &&} */}
+        <Route path="/area"  render={ () => <> <Header removeUser={this.removeUser}/> <AreaContainer /> </>}/>
       </Switch>
     </section>
   )
