22ad95faac30d4b62276d0abf5c7e50358ff694f
Nick Taylor <nickstaylor@gmail.com>
2020-05-13 22:53:05 -0600

    map function to display area cards and initial area card set up

--- src/components/AreaContainer/AreaContainer.js
+++ src/components/AreaContainer/AreaContainer.js
@@ -1,10 +1,23 @@
 import React from "react";
-import "./AreaContainer.css"; 
+import "./AreaContainer.css";
+import Area from '../Area/Area'
 
 
-const AreaContainer = () => {
+const AreaContainer = ({data}) => {
+  console.log('data', data)
+  const displayPics = data.map(area=>{
+    return (
+      <Area
+      name = {area.area}
+      image = {area.image}
+      />
+    )
+
+  })
 return(
-    <h2>Hello!</h2>
+  <div>
+    {displayPics}
+    </div>
 )
 }
 
