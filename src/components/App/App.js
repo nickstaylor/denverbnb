0ca5241f7e415c7c21a36d264c272b570949321e
December G <dgarnetsmith@gmail.com>
2020-05-15 04:03:33 -0600

    Update state to include information from all areas and location endpoints

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,42 +1,88 @@
-import React, { Component } from 'react';
-import './App.css';
-import Login from '../Login/Login'
-import Header from '../Header/Header'
-import AreaContainer from "../AreaContainer/AreaContainer"
+import React, { Component } from "react";
+import "./App.css";
+import Login from "../Login/Login";
+import Header from "../Header/Header";
+import AreaContainer from "../AreaContainer/AreaContainer";
 
-import { NavLink, Switch, Route, Redirect } from 'react-router-dom';
+import { NavLink, Switch, Route, Redirect } from "react-router-dom";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      user: '',
-    }
+      user: "",
+      areas: [],
+      error: "",
+    };
   }
+
+  componentDidMount = () => {
+    const array = [];
+    const baseURL = "https://vrad-api.herokuapp.com";
+    fetch(`${baseURL}/api/v1/areas`)
+      .then((response) => response.json())
+      .then((data) => {
+        let promises = data.areas.map((neighborhood) => {
+          return fetch(`${baseURL}${neighborhood.details}`)
+            .then((moreInfo) => moreInfo.json())
+            .then((singleNeighborhood) => {
+              const listingPromises = singleNeighborhood.listings.map(
+                (listing) => {
+                  return fetch(`${baseURL}${listing}`).then((listingData) =>
+                    listingData.json()
+                  );
+                }
+              );
+              return Promise.all(listingPromises)
+                .then((data) => {
+                  return {
+                    ...singleNeighborhood,
+                    listings: data,
+                  };
+                })
+                .then((areaInfo) => {
+                  array.push(areaInfo);
+                  console.log(array);
+                  return array;
+                })
+                .then((array) => this.setState({ areas: array }))
+                .catch((error) => this.setState({ error }));
+            });
+        });
+      });
+  };
 
   addUser = (person) => {
-    this.setState({user: person})
-  }
+    this.setState({ user: person });
+  };
 
   removeUser = () => {
-    this.setState({user: ''})
-  }
-
-  //updates state to include current user's name and purpose
+    this.setState({ user: "" });
+  };
 
   render() {
- 
-  return (
-    <section className="App">
-      
-      <Switch>
-        <Route path="/" exact render={ () => <Login addUser={this.addUser}/>}/>
-        {/* {this.state.user !== '' &&} */}
-        <Route path="/area"  render={ () => <> <Header removeUser={this.removeUser}/> <AreaContainer /> </>}/>
-      </Switch>
-    </section>
-  )
- }
+    return (
+      <section className="App">
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => <Login addUser={this.addUser} />}
+          />
+          {/* {this.state.user !== '' &&} */}
+          <Route
+            path="/area"
+            render={() => (
+              <>
+                {" "}
+                <Header removeUser={this.removeUser} /> <AreaContainer />{" "}
+              </>
+            )}
+          />
+        </Switch>
+      </section>
+    );
+  }
 }
 
 export default App;
