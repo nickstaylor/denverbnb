e8e013a33bd5abd3689c4363beca41d2885d9ffa
Quinn Elder <qre.turing@gmail.com>
2020-05-16 13:02:02 -0600

    Add Class companent for the Mian_Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -48,11 +48,9 @@ class App extends Component {
                 })
                 .then((areaInfo) => {
                   array.push(areaInfo);
-                  // console.log(array);
                   return array;
                 })
                 .then((completedData) => {
-                  // console.log(completedData);
                   completedData.forEach((area) => {
                     if (area.nickname === "RiNo") {
                       area.image = rino;
@@ -76,89 +74,17 @@ class App extends Component {
       });
   };
 
-  //     user: '',
-  //     areas: [],
-  //     favorites: [],
-  //     listings: []
-  //   }
-  // }
-
-  // componentDidMount(){
-  //   fetch("https://vrad-api.herokuapp.com/api/v1/areas")
-  //   .then(response => response.json())
-  //   .then(areaData => {
-  //     const areaPromises = areaData.areas.map(area=>{
-  //       return fetch(`https://vrad-api.herokuapp.com${area.details}`)
-  //       .then(response => response.json())
-  //       .then(neighborhood => {
-  //         const listingInfoPromises = neighborhood.listings.map(listing=>{
-  //           return fetch(`https://vrad-api.herokuapp.com${listing}`)
-  //           .then(response => response.json())
-  //           .then(info =>{
-  //             return {id: info.listing_id,
-  //                     area_id: info.area_id,
-  //                     name: info.name,
-  //                     address: info.address,
-  //                     details: info.details,
-  //                     dev_id: info.dev_id,
-  //                     area: info.area,
-  //                     db_connect: info.db_connect,
-  //                     isFavorited: false
-  //                     }
-  //           })
-  //         })
-  //         return Promise.all(listingInfoPromises).then(listingInfo=>{
-  //           return { area: area.area,
-  //             // id: listingInfo[0].area_id,
-  //             id: neighborhood.id,
-  //             location: neighborhood.location,
-  //             fullname: neighborhood.name,
-  //             about: neighborhood.about,
-  //             listings: listingInfo}
-  //           })
-  //       })
-  //     })
-  //     console.log('areaPromises', areaPromises)
-  //     Promise.all(areaPromises).then(completedAreaData => {
-  //       completedAreaData.forEach(area=>{
-  //         if (area.area === "RiNo"){
-  //             area.image = riNo
-  //         }
-  //         if (area.area === "Park Hill"){
-  //           area.image = parkHill
-  //         }
-  //         if (area.area === "LoHi"){
-  //             area.image = loHi
-  //         }
-  //         if (area.area === "Cap Hill"){
-  //           area.image = capHill
-  //         }
-  //       })
-
-  //       this.setState({areas: completedAreaData})
-  //     })
-  //   })
-  //   .catch(err => console.error(err))
-  // }
-
   addUser = (person) => {
     this.setState({ user: person });
   };
 
   removeUser = () => {
-    // return  (<Route path="/" exact render={ () => <Login addUser={this.addUser}/>}/>,
     this.setState({ user: "" });
     return <Redirect to="/" exact />;
   };
 
-  //updates state to include current user's name and purpose
 
   render() {
-    console.log('state', this.state.areas)
-    // if (!this.state.user){
-    //   return <Redirect to='/' exact />
-    //   // return <Route path="/" exact render={ () => <Login addUser={this.addUser}/>}/>
-    // }
     return (
       <section className="App">
         <Switch>
