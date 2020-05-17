2d646ae128f91fceca8d7d02fb2e9e689a76609d
Nick Taylor <nickstaylor@gmail.com>
2020-05-16 18:04:31 -0600

    add single big listing component

---
+++ src/components/SingleBigListing/SingleBigListing.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./SingleBigListing.css";
+// import starOutline from "../../../public/star-outline.svg"
+
+import { Link } from "react-router-dom";
+
+
+const SingleBigListing = () => {
+    // console.log('listing', props)
+
+    // <Link to={ `/areas/${area.id}/listings` }><button  className="area-button" value={area.id} >View Listings</button></Link>
+    return(
+  <div>
+  <h3>hello i'm the big listing</h3>
+  </div>
+
+  )
+}
+
+export default SingleBigListing;
