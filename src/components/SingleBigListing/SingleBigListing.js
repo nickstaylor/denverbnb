5d03898a6da16ca40c7665669c531ecb6a454a8e
Nick Taylor <nickstaylor@gmail.com>
2020-05-16 19:07:20 -0600

    add image pathways

--- src/components/SingleBigListing/SingleBigListing.js
+++ src/components/SingleBigListing/SingleBigListing.js
@@ -5,13 +5,17 @@ import "./SingleBigListing.css";
 import { Link } from "react-router-dom";
 
 
-const SingleBigListing = () => {
-    // console.log('listing', props)
+const SingleBigListing = (props) => {
+  const imagePathA = `/repoImages/${props.listing_id}_a.jpg`;
+  const imagePathB = `/repoImages/${props.listing_id}_b.jpg`;
+  const imagePathC = `/repoImages/${props.listing_id}_c.jpg`;
+    console.log('SingleBigListing', props)
 
     // <Link to={ `/areas/${area.id}/listings` }><button  className="area-button" value={area.id} >View Listings</button></Link>
     return(
   <div>
   <h3>hello i'm the big listing</h3>
+  <img src={imagePathB} />
   </div>
 
   )
