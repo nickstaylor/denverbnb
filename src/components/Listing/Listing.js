779700cb3e70579dbf154832e34a59ae418f0745
Quinn Elder <qre.turing@gmail.com>
2020-05-17 11:20:02 -0600

    syntax change

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -13,7 +13,7 @@ return(
   <div className="individual-listing">
     <h3>{props.data.name}</h3>
     <img className="listing-image" src = {props.imageA} />
-    <section class="listing-buttons">
+    <section className="listing-buttons">
     <Link className="listing-button" to={`/areas/${props.data.area_id}/listings/${props.data.listing_id}`}>See Listing</Link>
     <button className="listing-button" onClick={props.favoriteListing} value={props.data.listing_id} >Favorite Me!</button>
     </section>
