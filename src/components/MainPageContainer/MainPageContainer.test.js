18b1fa5574bb7f3507d18f7c32fae612f152f451
Nick Taylor <nickstaylor@gmail.com>
2020-05-19 17:47:04 -0600

    slight syntax corrections

--- src/components/MainPageContainer/MainPageContainer.test.js
+++ src/components/MainPageContainer/MainPageContainer.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from "react-router-dom";
+// import ( getAreas ) from ''
 
 import MainPageContainer from './MainPageContainer';
 
 describe('MainPageContainer', () => {
-  
+
   it('should render the MainPageContainer in the document', () => {
   const {getByTitle} = render(  <BrowserRouter>
         <MainPageContainer />
