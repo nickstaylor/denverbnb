916abc8938f8eb8fff6552eda74fc37774f269e2
Nick Taylor <nickstaylor@gmail.com>
2020-05-20 16:53:00 -0600

    adjust apiCalls, add favorites function to single big listing, and begin clean up

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,19 +1,32 @@
-
 const baseURL = "https://vrad-api.herokuapp.com";
 
-export const getAreas = () => {
-  return fetch(`${baseURL}/api/v1/areas`)
-    .then((response) => response.json())
-
+export const fetchingApi = async () => {
+  const response = await fetch(`${baseURL}/api/v1/areas`)
+  const data  = await response.json()
+  const newData = await data.areas.map(async (area) => {
+    const promiseArea = await fetch(`${baseURL}${area.details}`)
+    const areaData = await promiseArea.json()
+    let newArea = {
+      ...areaData,
+      nickname: area.area
+    }
+    return newArea
+    })
+  return Promise.all(newData)
 }
 
-export const getNeighborhood = (value) => {
-  return fetch(`${baseURL}${value}`)
-    .then((moreInfo) => moreInfo.json())
-}
-
-export const getListings = (value) => {
-  return fetch(`${baseURL}${value}`)
-  .then((listingData) =>
-    listingData.json())
+export const getIndividualListing = async (areaArray) => {
+    let map = areaArray.map(async (area) => {
+    let listings = await area.listings.map( async (listing) => {
+      return await fetch(`${baseURL}${listing}`).then(listingObj => listingObj.json()).then(listingObject => listingObject)
+    })
+    let newListing= await (Promise.all(listings))
+    let newArea = {
+      ...area,
+      listings: newListing,
+      image: '',
+    }
+    return newArea
+  })
+  return Promise.all(map)
 }
