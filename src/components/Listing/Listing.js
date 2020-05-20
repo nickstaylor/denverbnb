88d2df72fa29ee30f577af3c2384d43ef104e97d
Nick Taylor <nickstaylor@gmail.com>
2020-05-20 16:53:20 -0600

    adjust apiCalls, add favorites function to single big listing, and begin clean up

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -14,29 +14,27 @@ class Listing extends React.Component {
       starFilled: starFilled,
       starOutline: starOutline,
       starImage: (this.props.favorite ? "starFilled" : "starOutline")
-
-
     }
   }
 
 
   favoriteThisListing = (event) => {
     let id = event.target.id
-    let fromFavorites = false;
-    if (this.props.comingFromFavorites){
-      fromFavorites = true
-    }
-    let imageName;
     const favoritedListing = {
       ...this.props.data,
       image: this.props.imageA,
     }
 
+    let imageName;
     this.state.isFavorited ?
     imageName = "starOutline" :
     imageName = "starFilled"
     this.setState({isFavorited: !this.state.isFavorited,
                     starImage: imageName})
+    let fromFavorites = false;
+    if (this.props.comingFromFavorites){
+      fromFavorites = true
+    }
     this.props.favoriteListing(favoritedListing, fromFavorites)
     if (fromFavorites){
       this.props.updateFavoriteState(id)
@@ -45,7 +43,6 @@ class Listing extends React.Component {
 
 
   render() {
-    console.log(this.props.comingFromFavorites)
   return(
     <div className="individual-listing">
       <img id ={this.props.data.listing_id} src = {this.state[this.state.starImage]} className="favorite-star" alt="favorite"
